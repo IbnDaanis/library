0a17bc5a589dfd4ca4b00214ad360bb66d09046e
Ikraam <IbnDaanis@Gmail.com>
2021-01-08 18:22:45 +0000

    Refactored app using classes

--- js/app.js
+++ js/app.js
@@ -13,10 +13,82 @@ setTimeout(() => {
   addABookHere.style.opacity = '0'
 }, 8000)
 
-let myLibrary = localStorage.getItem('myLibrary')
-  ? JSON.parse(localStorage.getItem('myLibrary'))
-  : []
-if (myLibrary.length > 1) addABookHere.style.display = ' none'
+class Library {
+  constructor(library) {
+    this.library = library
+  }
+  addBookToLibrary = book => {
+    this.library.push(book)
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
+  }
+  removeBook = id => {
+    this.library = this.library.filter(book => book.id !== id)
+    localStorage.setItem('myLibrary', JSON.stringify(this.library))
+  }
+  toggleIsRead = item => {
+    item.isRead = !item.isRead
+    localStorage.setItem('myLibrary', JSON.stringify(this.library))
+    addBookToDom()
+  }
+  addBookToDom = (library = this.library) => {
+    libraryContainer.innerHTML = ''
+    library.forEach(book => {
+      const div = document.createElement('div')
+      div.classList.add('book-item')
+      book.isRead && div.classList.add('read')
+      div.setAttribute('data-id', book.id)
+      const title = document.createElement('h2')
+      title.textContent = book.title
+      const author = document.createElement('p')
+      author.textContent = `Author: ${book.author}`
+      const pages = document.createElement('p')
+      pages.textContent = `Pages: ${book.pages}`
+      const read = document.createElement('p')
+      read.textContent = `${book.isRead ? 'Read' : 'Not read'}`
+      const toggleRead = document.createElement('p')
+      toggleRead.classList.add('toggle-read')
+      toggleRead.style.opacity = '0'
+      toggleRead.textContent = `Click to ${
+        book.isRead ? 'mark as unread' : 'mark as read'
+      }`
+      const removeBookButton = document.createElement('button')
+      removeBookButton.textContent = 'X'
+      removeBookButton.onclick = () => {
+        this.removeBook(book.id)
+        this.addBookToDom(this.library)
+      }
+
+      const isRead = document.createElement('div')
+      isRead.classList.add('is-read', 'toggle-read')
+      const toggle = document.createElement('label')
+      toggle.textContent = 'Book read?'
+      toggle.for = `thisIsRead${book.id}`
+      toggle.classList.add('toggle')
+      isRead.appendChild(toggle)
+      const checkbox = document.createElement('input')
+      checkbox.type = 'checkbox'
+      checkbox.id = `thisIsRead${book.id}`
+      checkbox.classList.add('is-read-checkbox')
+      checkbox.onchange = () => {
+        book.isRead = !book.isRead
+        localStorage.setItem('myLibrary', JSON.stringify(this.library))
+        this.addBookToDom(this.library)
+      }
+      checkbox.checked = book.isRead
+      toggle.appendChild(checkbox)
+      const toggleFill = document.createElement('span')
+      toggleFill.classList.add('toggle__fill')
+      toggle.appendChild(toggleFill)
+      div.appendChild(title)
+      div.appendChild(author)
+      div.appendChild(pages)
+      div.appendChild(read)
+      div.appendChild(removeBookButton)
+      div.appendChild(isRead)
+      libraryContainer.appendChild(div)
+    })
+  }
+}
 
 class Book {
   constructor(author, title, pages, isRead) {
@@ -30,83 +102,13 @@ class Book {
   }
 }
 
-const addBookToLibrary = (author, title, pages, isRead) => {
-  const newBook = new Book(author, title, pages, isRead)
-  myLibrary.push(newBook)
-  localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
-}
+let libraryArr = localStorage.getItem('myLibrary')
+  ? JSON.parse(localStorage.getItem('myLibrary'))
+  : []
 
-const removeBook = id => {
-  myLibrary = myLibrary.filter(book => book.id !== id)
-  localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
-}
-
-const toggleIsRead = item => {
-  item.isRead = !item.isRead
-  localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
-  addBookToDom(myLibrary)
-}
-
-const addBookToDom = library => {
-  libraryContainer.innerHTML = ''
-  library.forEach(book => {
-    const div = document.createElement('div')
-    div.classList.add('book-item')
-    book.isRead && div.classList.add('read')
-    div.setAttribute('data-id', book.id)
-    const title = document.createElement('h2')
-    title.textContent = book.title
-    const author = document.createElement('p')
-    author.textContent = `Author: ${book.author}`
-    const pages = document.createElement('p')
-    pages.textContent = `Pages: ${book.pages}`
-    const read = document.createElement('p')
-    read.textContent = `${book.isRead ? 'Read' : 'Not read'}`
-    const toggleRead = document.createElement('p')
-    toggleRead.classList.add('toggle-read')
-    toggleRead.style.opacity = '0'
-    toggleRead.textContent = `Click to ${
-      book.isRead ? 'mark as unread' : 'mark as read'
-    }`
-    const removeBookButton = document.createElement('button')
-    removeBookButton.textContent = 'X'
-    removeBookButton.onclick = () => {
-      removeBook(book.id)
-      addBookToDom(myLibrary)
-    }
-
-    const isRead = document.createElement('div')
-    isRead.classList.add('is-read', 'toggle-read')
-    const toggle = document.createElement('label')
-    toggle.textContent = 'Book read?'
-    toggle.for = `thisIsRead${book.id}`
-    toggle.classList.add('toggle')
-    isRead.appendChild(toggle)
-    const checkbox = document.createElement('input')
-    checkbox.type = 'checkbox'
-    checkbox.id = `thisIsRead${book.id}`
-    checkbox.classList.add('is-read-checkbox')
-    checkbox.onchange = () => {
-      book.isRead = !book.isRead
-      localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
-      addBookToDom(myLibrary)
-    }
-    checkbox.checked = book.isRead
-    toggle.appendChild(checkbox)
-    const toggleFill = document.createElement('span')
-    toggleFill.classList.add('toggle__fill')
-    toggle.appendChild(toggleFill)
-
-    div.appendChild(title)
-    div.appendChild(author)
-    div.appendChild(pages)
-    div.appendChild(read)
-    div.appendChild(removeBookButton)
-    div.appendChild(isRead)
-    libraryContainer.appendChild(div)
-  })
-}
-addBookToDom(myLibrary)
+const myLibrary = new Library(libraryArr)
+if (myLibrary.library.length > 1) addABookHere.style.display = ' none'
+myLibrary.addBookToDom()
 
 addBook.addEventListener('click', () => {
   modal.classList.toggle('closed')
@@ -114,8 +116,14 @@ addBook.addEventListener('click', () => {
 
 form.addEventListener('submit', e => {
   e.preventDefault()
-  addBookToLibrary(author.value, title.value, pages.value, isRead.checked)
-  addBookToDom(myLibrary)
+  const newBook = new Book(
+    author.value,
+    title.value,
+    pages.value,
+    isRead.checked
+  )
+  myLibrary.addBookToLibrary(newBook)
+  myLibrary.addBookToDom(myLibrary.library)
   modal.classList.toggle('closed')
   author.value = ''
   title.value = ''
